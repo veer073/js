557f8837e03fe0f9b1567fdde2e0293550e879c6
veer073 <113500751+veer073@users.noreply.github.com>
2024-02-20 13:40:08 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score="76mnm"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33"=> 33
+// "33abc"=> NaN
+//true =1,false=0
+
+let isLoggedIn=0
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+
+
+/*
+1=>true,0=>false
+""=>false
+"veer"=>true
+ */
