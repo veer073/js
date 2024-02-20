e543259c729eaf88f04895d76ea4917df97ca258
veer073 <113500751+veer073@users.noreply.github.com>
2024-02-20 16:52:16 +0000

    operator

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -25,3 +25,58 @@ console.log(booleanisLoggedIn);
 ""=>false
 "veer"=>true
  */
+
+
+
+
+
+
+
+
+
+
+
+
+// *************operations*****************
+
+
+let value=3
+let negvalue= -value
+
+console.log(negvalue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1="Veer "
+let str2="Thakur"
+console.log(str1+str2);
+
+
+console.log("1"+2);
+console.log(1+"2");
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+
+let num1,num2,num3
+num1=num2=num3=2+2  //please dont prefer these types of codes
+
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+
