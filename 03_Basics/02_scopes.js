8d96dd47a5c45e60021c6f48111352c07960f7bd
veer073 <113500751+veer073@users.noreply.github.com>
2024-02-22 18:02:32 +0000

    scope start

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,10 @@
+if(true){
+    let a=10
+    const b=20
+    var c=30
+}
+
+
+// console.log(a);---> let is not out of scope
+// console.log(b);---> const is also not out of scope
+// console.log(c);--->var is printed out of scope
