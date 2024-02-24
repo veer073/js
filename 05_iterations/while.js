51e063728d14c847cfdab4980ce8b76259cdaafd
veer073 <113500751+veer073@users.noreply.github.com>
2024-02-24 19:00:38 +0000

    while and dowhile loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,33 @@
+//************While loop*************
+
+// while (condition) {
+    
+// }
+let index=0
+while (index<=10) {
+    console.log(`Value of index is ${index}`)
+    index+=2;
+}
+
+let myArray=["flash","batman","superman"]
+
+let arr=0
+while (arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+} 
+    
+
+//************do While loop*************
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score+=3
+} while (score<10);
+
+
+let scores=61
+do {
+    console.log(`Score is ${scores}`);
+    scores+=3
+} while (scores<10);
